cc2f7d5c1999a430e2fb06d3ef624f4ef81e4ca5
Masayoshi Sekimura <sekimura@gmail.com>
2015-01-12 13:39:42 -0800

    style: gofmt

--- normalizeurl.go
+++ normalizeurl.go
@@ -1,59 +1,59 @@
 package normalizeurl
 
 import (
-    "net/url"
+	"fmt"
+	"golang.org/x/net/idna"
+	"net/url"
 	"strings"
-    "fmt"
-    "golang.org/x/net/idna"
 )
 
 var (
-    DefaultPorts = map[string]int {
-        "http": 80,
-        "https": 443,
-        "ftp": 21,
-    }
+	DefaultPorts = map[string]int{
+		"http":  80,
+		"https": 443,
+		"ftp":   21,
+	}
 )
 
 func normalize(s string) (string, error) {
 	s = strings.TrimSpace(s)
 	if strings.HasPrefix(s, "//") {
-        s = "http:" + s
-    }
+		s = "http:" + s
+	}
 
 	u, err := url.Parse(s)
 	if err != nil {
 		return s, err
 	}
 
-    if u.Scheme == "" {
-        // Ugh...
-        u, err = url.Parse("http://" + s)
-        if err != nil {
-            return s, err
-        }
-    }
+	if u.Scheme == "" {
+		// Ugh...
+		u, err = url.Parse("http://" + s)
+		if err != nil {
+			return s, err
+		}
+	}
 
-    p, ok := DefaultPorts[u.Scheme]
-    if ok {
-        u.Host = strings.TrimSuffix(u.Host, fmt.Sprintf(":%d", p))
-    } 
+	p, ok := DefaultPorts[u.Scheme]
+	if ok {
+		u.Host = strings.TrimSuffix(u.Host, fmt.Sprintf(":%d", p))
+	}
 
-    got, err := idna.ToUnicode(u.Host)
-    if err != nil {
-        return got, err
-    } else {
-        u.Host = got
-    }
+	got, err := idna.ToUnicode(u.Host)
+	if err != nil {
+		return got, err
+	} else {
+		u.Host = got
+	}
 
-    u.Host = strings.TrimPrefix(u.Host, "www.")
+	u.Host = strings.TrimPrefix(u.Host, "www.")
 
-    v := u.Query()
-    u.RawQuery = v.Encode()
-    u.RawQuery, _ = url.QueryUnescape(u.RawQuery)
+	v := u.Query()
+	u.RawQuery = v.Encode()
+	u.RawQuery, _ = url.QueryUnescape(u.RawQuery)
 
 	h := u.String()
 	h = strings.TrimSuffix(h, "/")
 
-    return h, nil
+	return h, nil
 }
--- normalizeurl_test.go
+++ normalizeurl_test.go
@@ -1,30 +1,30 @@
 package normalizeurl
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestNormalize(t *testing.T) {
-    tests := []struct{
-        value string
-        expected string
-    }{
-        {"www.sindresorhus.com", "http://sindresorhus.com"},
-        {"sindresorhus.com", "http://sindresorhus.com"},
-        {"HTTP://sindresorhus.com", "http://sindresorhus.com"},
-        {"//sindresorhus.com", "http://sindresorhus.com"},
-        {"http://sindresorhus.com", "http://sindresorhus.com"},
-        {"http://sindresorhus.com:80", "http://sindresorhus.com"},
-        {"https://sindresorhus.com:443", "https://sindresorhus.com"},
-        {"ftp://sindresorhus.com:21", "ftp://sindresorhus.com"},
-        {"http://www.sindresorhus.com", "http://sindresorhus.com"},
-        {"www.sindresorhus.com", "http://sindresorhus.com"},
-        {"http://sindresorhus.com/foo/", "http://sindresorhus.com/foo"},
-        {"sindresorhus.com/?foo=bar%20baz", "http://sindresorhus.com/?foo=bar baz"},
-        {"http://sindresorhus.com/?", "http://sindresorhus.com"},
-        {"http://xn--xample-hva.com", "http://êxample.com"},
-        {"http://sindresorhus.com/?b=bar&a=foo", "http://sindresorhus.com/?a=foo&b=bar"},
-    }
+	tests := []struct {
+		value    string
+		expected string
+	}{
+		{"www.sindresorhus.com", "http://sindresorhus.com"},
+		{"sindresorhus.com", "http://sindresorhus.com"},
+		{"HTTP://sindresorhus.com", "http://sindresorhus.com"},
+		{"//sindresorhus.com", "http://sindresorhus.com"},
+		{"http://sindresorhus.com", "http://sindresorhus.com"},
+		{"http://sindresorhus.com:80", "http://sindresorhus.com"},
+		{"https://sindresorhus.com:443", "https://sindresorhus.com"},
+		{"ftp://sindresorhus.com:21", "ftp://sindresorhus.com"},
+		{"http://www.sindresorhus.com", "http://sindresorhus.com"},
+		{"www.sindresorhus.com", "http://sindresorhus.com"},
+		{"http://sindresorhus.com/foo/", "http://sindresorhus.com/foo"},
+		{"sindresorhus.com/?foo=bar%20baz", "http://sindresorhus.com/?foo=bar baz"},
+		{"http://sindresorhus.com/?", "http://sindresorhus.com"},
+		{"http://xn--xample-hva.com", "http://êxample.com"},
+		{"http://sindresorhus.com/?b=bar&a=foo", "http://sindresorhus.com/?a=foo&b=bar"},
+	}
 
 	for i := range tests {
 		actual, err := normalize(tests[i].value)
